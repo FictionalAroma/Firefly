b278a190f95d6a69cf6c16487d65dcca404989a0
Triss-exe <trissgames.exe@gmail.com>
2024-05-27 01:07:40 +1000

    switch from bugs to necro map started

--- Scripts/firefly.gd
+++ Scripts/firefly.gd
@@ -12,7 +12,7 @@ func _physics_process(delta):
 	var distance_left = position.distance_to(click_position)
 	var current_agent_position: Vector2 = global_position
 	var next_path_position: Vector2 = navigation_agent.get_next_path_position()
-	if Input.is_action_just_pressed("right_click"):
+	if Input.is_action_just_pressed("move-to-point"):
 		click_position = get_global_mouse_position()
 		look_at(click_position)
 		animated_sprite.play("fly")
